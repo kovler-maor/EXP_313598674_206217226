cf9953d7950ce1a1804bdabb2fe759c367d20514
kovler-maor <kovlermaor@gmail.com>
2022-01-11 20:49:46 +0200

    Q 11/01

---
+++ src/Qnode.java
@@ -0,0 +1,13 @@
+public class Qnode {
+    public GraphNode graphNode;
+    public Qnode next;
+
+    public Qnode(GraphNode node){
+        this.graphNode = node;
+        this.next = null;
+    }
+
+
+
+
+}
--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,31 @@
 public class Queue {
+    public Qnode Head;
+    public Qnode Tail;
+
+    Queue(GraphNode node){
+        this.Head = null;
+        this.Tail = null;
+    }
+
+    void Enqueue(GraphNode node){
+        Qnode temp = new Qnode(node);
+        if(this.Tail == null){
+            this.Head = this.Tail = temp;
+        }
+    }
+
+    void Dequeue(){
+        if(this.Head == null){
+            return;
+        }
+        Qnode temp = this.Head;
+        this.Head = this.Head.next;
+        if(this.Head == null){
+            this.Tail = null;
+        }
+
+    }
+
+
 
 }
